1aa89c5e2183037b8d322cf230a718467795fda1
Xander Flood <xander.t.flood@gmail.com>
2019-11-08 18:44:08 -0500

    use %v not %s

--- docker/resource_docker_secret_test.go
+++ docker/resource_docker_secret_test.go
@@ -96,10 +96,10 @@ func TestAccDockerSecret_labels(t *testing.T) {
 				}
 				`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%s.label", hashStringLabel("test1")), "test1"),
-					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%s.value", hashStringLabel("test1")), "foo"),
-					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%s.label", hashStringLabel("test2")), "test1"),
-					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%s.value", hashStringLabel("test2")), "bar"),
+					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%v.label", hashStringLabel("test1")), "test1"),
+					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%v.value", hashStringLabel("test1")), "foo"),
+					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%v.label", hashStringLabel("test2")), "test1"),
+					resource.TestCheckResourceAttr("docker_secret.foo", fmt.Sprintf("labels.%v.value", hashStringLabel("test2")), "bar"),
 				),
 			},
 		},
