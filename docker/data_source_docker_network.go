1404102e70d152b513a11b649a6b406626c33212
Philippe Vienne <PhilippeGeek@gmail.com>
2020-02-01 13:09:36 +0100

    fix: corrects IPAM config read on the data provider (#229)

--- docker/data_source_docker_network.go
+++ docker/data_source_docker_network.go
@@ -40,8 +40,7 @@ func dataSourceDockerNetwork() *schema.Resource {
 
 			"ipam_config": &schema.Schema{
 				Type:     schema.TypeSet,
-				Optional: true,
-				ForceNew: true,
+				Computed: true,
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"subnet": &schema.Schema{
@@ -79,6 +78,8 @@ func dataSourceDockerNetwork() *schema.Resource {
 	}
 }
 
+type ipamMap map[string]interface{}
+
 func dataSourceDockerNetworkRead(d *schema.ResourceData, meta interface{}) error {
 
 	name, nameOk := d.GetOk("name")
@@ -102,7 +103,16 @@ func dataSourceDockerNetworkRead(d *schema.ResourceData, meta interface{}) error
 	d.Set("driver", network.Driver)
 	d.Set("options", network.Options)
 	d.Set("internal", network.Internal)
-	d.Set("imap_config", network.IPAM)
+	ipam := make([]ipamMap, len(network.IPAM.Config))
+	for i, config := range network.IPAM.Config {
+		ipam[i] = ipamMap{
+			"subnet":      config.Subnet,
+			"gateway":     config.Gateway,
+			"aux_address": config.AuxAddress,
+			"ip_range":    config.IPRange,
+		}
+	}
+	err = d.Set("ipam_config", ipam)
 
 	return nil
 }
--- docker/data_source_docker_network_test.go
+++ docker/data_source_docker_network_test.go
@@ -1,6 +1,9 @@
 package docker
 
 import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/terraform"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -15,6 +18,7 @@ func TestAccDockerNetworkDataSource_basic(t *testing.T) {
 				Config: testAccDockerNetworkDataSourceConfig,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.docker_network.bridge", "name", "bridge"),
+					testAccDockerNetworkDataSourceIPAMRead,
 					resource.TestCheckResourceAttr("data.docker_network.bridge", "driver", "bridge"),
 					resource.TestCheckResourceAttr("data.docker_network.bridge", "internal", "false"),
 					resource.TestCheckResourceAttr("data.docker_network.bridge", "scope", "local"),
@@ -22,6 +26,22 @@ func TestAccDockerNetworkDataSource_basic(t *testing.T) {
 			},
 		},
 	})
+}
+
+func testAccDockerNetworkDataSourceIPAMRead(state *terraform.State) error {
+	bridge := state.RootModule().Resources["data.docker_network.bridge"]
+	if bridge == nil {
+		return fmt.Errorf("unable to find data.docker_network.bridge")
+	}
+	attr := bridge.Primary.Attributes["ipam_config.#"]
+	numberOfReadConfig, err := strconv.Atoi(attr)
+	if err != nil {
+		return err
+	}
+	if numberOfReadConfig < 1 {
+		return fmt.Errorf("unable to find any ipam_config")
+	}
+	return nil
 }
 
 const testAccDockerNetworkDataSourceConfig = `
