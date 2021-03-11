c7b26aae318acf22c1d6da05f43faa06eea93715
Manuel Vogel <mavogel@posteo.de>
2021-03-11 21:20:36 +0100

    feat: set descriptions to markdown and show an example builder

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -16,6 +16,22 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
+func init() {
+	// Set descriptions to support markdown syntax, this will be used in document generation
+	// and the language server.
+	schema.DescriptionKind = schema.StringMarkdown
+
+	// Customize the content of descriptions when output. For example you can add defaults on
+	// to the exported descriptions if present.
+	// schema.SchemaDescriptionBuilder = func(s *schema.Schema) string {
+	// 	desc := s.Description
+	// 	if s.Default != nil {
+	// 		desc += fmt.Sprintf(" Defaults to `%v`.", s.Default)
+	// 	}
+	// 	return strings.TrimSpace(desc)
+	// }
+}
+
 // New creates the Docker provider
 func New(version string) func() *schema.Provider {
 	return func() *schema.Provider {
