1868ade628ba8f9459a29c2a9cdbbba13766d43d
Manuel Vogel <mavogel@posteo.de>
2021-05-24 20:49:23 +0200

    fix: remove unused volume label test function

--- internal/provider/resource_docker_volume_test.go
+++ internal/provider/resource_docker_volume_test.go
@@ -101,12 +101,3 @@ func TestAccDockerVolume_labels(t *testing.T) {
 		},
 	})
 }
-
-func testAccVolumeLabel(volume *types.Volume, name string, value string) resource.TestCheckFunc { //nolint:deadcode,unused
-	return func(s *terraform.State) error {
-		if volume.Labels[name] != value {
-			return fmt.Errorf("Bad value for label '%s': %s", name, volume.Labels[name])
-		}
-		return nil
-	}
-}
