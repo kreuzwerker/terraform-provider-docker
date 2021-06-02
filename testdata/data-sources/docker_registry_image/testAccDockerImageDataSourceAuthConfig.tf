461ca4b99904d97f5eb254d62e4da1618b9dcb1b
Manuel Vogel <mavogel@posteo.de>
2021-06-02 23:37:13 +0200

    chore: rum terraform fmt

--- testdata/data-sources/docker_registry_image/testAccDockerImageDataSourceAuthConfig.tf
+++ testdata/data-sources/docker_registry_image/testAccDockerImageDataSourceAuthConfig.tf
@@ -1,11 +1,11 @@
 provider "docker" {
-	alias = "private"
-	registry_auth {
-		address = "%s"
-	}
+  alias = "private"
+  registry_auth {
+    address = "%s"
+  }
 }
 data "docker_registry_image" "foobar" {
-	provider = "docker.private"
-	name     = "%s"
-	insecure_skip_verify = true
+  provider             = "docker.private"
+  name                 = "%s"
+  insecure_skip_verify = true
 }
