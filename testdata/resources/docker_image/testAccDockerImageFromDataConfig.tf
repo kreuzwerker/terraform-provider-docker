8ddf39054e496c42e383485b2b01c53625036d10
Martin Wentzel <martin.wentzel@kreuzwerker.de>
2022-06-22 21:31:58 +0200

    fix: Use image version which are available on arm64.

--- testdata/resources/docker_image/testAccDockerImageFromDataConfig.tf
+++ testdata/resources/docker_image/testAccDockerImageFromDataConfig.tf
@@ -1,5 +1,5 @@
 data "docker_registry_image" "foobarbaz" {
-  name = "alpine:3.1"
+  name = "alpine:3.16.0"
 }
 resource "docker_image" "foobarbaz" {
   name          = data.docker_registry_image.foobarbaz.name
