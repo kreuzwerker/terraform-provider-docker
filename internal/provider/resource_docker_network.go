a67af35162dc0e6392663f3d38649e1e48f9335c
Lenox, Joseph D (Collins) <Joseph.Lenox@collins.com>
2022-12-21 10:04:59 -0600

    docs: Revise IPAM options documentation to make more clear as to what it does.

--- internal/provider/resource_docker_network.go
+++ internal/provider/resource_docker_network.go
@@ -98,7 +98,7 @@ func resourceDockerNetwork() *schema.Resource {
 			},
 			"ipam_options": {
 				Type:        schema.TypeMap,
-				Description: "Only available with bridge networks. See [bridge options docs](https://docs.docker.com/engine/reference/commandline/network_create/#bridge-driver-options) for more details.",
+				Description: "Provide explicit options to the IPAM driver. Valid options vary with `ipam_driver` and refer to that driver's documentation for more details.",
 				Optional:    true,
 				ForceNew:    true,
 				Computed:    true,
